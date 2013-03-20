607572e5fabd17d41a71a97322d6fd4a3f3c7bd8
Eelizabeth <eli_bere@hotmail.com>
2013-03-20 13:22:35 -0600

    Create persona1.cs

---
+++ persona1.cs
@@ -0,0 +1,25 @@
+using System;
+namespace practica
+{
+  public class Persona
+	{
+		public string codigo;
+		public string nombre;
+		public string direccion;
+		public string facebook;
+		public string telefono;
+		public string edad;
+		public string email;
+
+		
+		public Persona(){
+			this.codigo = "";
+			this.nombre = "";
+			this.direccion = "";
+			this.facebook = "";
+			this.telefono = "";
+			this.edad = "";
+			this.email = "";
+		}
+	}
+}
